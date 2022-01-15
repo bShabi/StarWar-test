12adf7f2cefc073d275b9cce94f2791cafcbadff
Ben Shabi <bshabi1994@gmail.com>
2022-01-15 18:15:25 +0200

    change async logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,55 +2,57 @@ import { useState, useEffect, FunctionComponent } from 'react';
 import { PartOne } from './components/TikalTestPartOne';
 import { PartTwo } from './components/TikalTestPartTwo';
 import axios from 'axios';
+
 export interface IVehiclePage {
   next: string;
   results: IVehicle[];
 }
+
 export interface IVehicle {
   name?: string;
-  pilots?: string[];
+  pilots: string[];
   pilotNames?: string[];
   population: number;
   homeworld?: IPlanetPage[];
 }
+
 interface IPlanetPage {
   name: string;
   population: any;
 }
+
 interface IPepolePage {
   name: string;
   homeworld: string;
 }
+
 export type VehiclesType = [IVehicle];
 //setVehicleResult
 const App: FunctionComponent<{ initial?: IVehicle }> = () => {
-  const [vehicleResult, setVehicleResult] = useState<IVehicle | undefined>(
-    undefined
-  );
-  const [bestVehicle, setBestVehicle] = useState<IVehicle>();
+  const [vehicleResult, setVehicleResult] = useState<IVehicle | null>(null);
+  const [bestVehicle, setBestVehicle] = useState<IVehicle | null>(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (vehicleResult) return;
+
     async function fetchVehicle() {
-      console.log('in');
       if (vehicleResult) return;
       const resultVehicle: IVehicle[] = await getVehicleResult(
         'https://swapi.py4e.com/api/vehicles'
       );
-      const vehiclePopulation: IVehicle[] = await prepeerData(resultVehicle);
-      const highestVehicle: IVehicle = await getHightByPopultion(
-        vehiclePopulation
+      const vehiclePopulation: null | IVehicle[] = await prepareData(
+        resultVehicle
       );
-      console.log('highestVehicle', highestVehicle);
+      const highestVehicle: null | IVehicle =
+        getByHighestPopulation(vehiclePopulation);
       setBestVehicle(highestVehicle);
       setVehicleResult(highestVehicle);
       // setLoading(true);
     }
+
     fetchVehicle();
   }, []);
-
-  (async () => {})();
 
   const getVehicleResult = async (pathUrl: string) => {
     var nextUrl = pathUrl;
@@ -69,32 +71,35 @@ const App: FunctionComponent<{ initial?: IVehicle }> = () => {
 
     return result;
   };
-  const prepeerData = async (
+
+  const prepareData = async (
     vehicleResult: IVehicle[]
-  ): Promise<IVehicle[]> => {
-    vehicleResult.map(async (vehicle: IVehicle): Promise<any> => {
+  ): Promise<null | IVehicle[]> => {
+    for (const vehicle of vehicleResult) {
       vehicle.population = 0;
       vehicle.homeworld = [];
       vehicle.pilotNames = [];
-      vehicle.pilots?.forEach(async (pialotUrl: string) => {
-        var homeWorld: IPepolePage = await prepeerHomeWorldToVehicle(pialotUrl);
-        if (!homeWorld) {
-          return;
-        }
+      for (const url of vehicle.pilots) {
+        const homeWorld: null | IPepolePage = await prepareHomeWorldToVehicle(
+          url
+        );
+
+        if (!homeWorld) return null;
+
         const homeWorldPage: IPlanetPage = await reducerPopulation(
           homeWorld.homeworld
         );
-        console.log('homeWorldPage', homeWorldPage);
         vehicle.homeworld?.push(homeWorldPage);
 
         vehicle.population += homeWorldPage.population;
         vehicle.pilotNames?.push(homeWorld.name);
-      });
-    });
-
+      }
+    }
     return vehicleResult;
   };
-  const prepeerHomeWorldToVehicle = async (pathUrl: string): Promise<any> => {
+  const prepareHomeWorldToVehicle = async (
+    pathUrl: string
+  ): Promise<null | IPepolePage> => {
     const res = await axios.get<IPepolePage>(pathUrl);
     const planetPage: IPepolePage = {
       name: res.data.name,
@@ -108,18 +113,25 @@ const App: FunctionComponent<{ initial?: IVehicle }> = () => {
   };
   const reducerPopulation = async (pathUrl: string): Promise<IPlanetPage> => {
     const res = await axios.get<IPlanetPage>(pathUrl);
-    const homeWorldPage: IPlanetPage = {
+    return {
       name: res.data.name,
       population:
         res.data.population === 'unknown' ? 0 : Number(res.data.population),
     };
-    return homeWorldPage;
   };
 
-  const getHightByPopultion = async (vehicles: IVehicle[]): Promise<any> => {
-    return vehicles.reduce(function (prev, current) {
-      return prev.population > current.population ? prev : current;
-    });
+  const getByHighestPopulation = (
+    vehicles: IVehicle[] | null
+  ): null | IVehicle => {
+    let highest: null | IVehicle = null;
+
+    if (vehicles)
+      vehicles.forEach((vehicle) => {
+        if (!highest) highest = vehicle;
+        else if (vehicle.population > highest.population) highest = vehicle;
+      });
+
+    return highest;
   };
   // (async () => {})();
 
